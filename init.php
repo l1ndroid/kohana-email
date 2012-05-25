3c275167447ce53bbe5327d36e1f9f17fb1f1987
Thomas Birch <thomas.birch@kiddivouchers.com>
2012-05-25 15:08:53 +0100

    Add composer def, and update init

--- init.php
+++ init.php
@@ -1,4 +1,4 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 // Autoloading for Swiftmailer
-require Kohana::find_file('vendor/swiftmailer', 'lib/swift_required');
+require Kohana::find_file('vendor/swiftmailer/swiftmailer', 'lib/swift_required');
